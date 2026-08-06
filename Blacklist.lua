11c0d9571c0575d452ef0bbf50741aff29d98113
Astro-1-Verse <diandreharris7@gmail.com>
2026-08-06 11:12:14 -0400

    Blacklist user idk

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,4 +1,5 @@
 return({
     {hwid = "57E0C2C6-BCBB-4108-B845-D7577986DD61", ip = "142.129.208.4", user_id = "faggot"}, --<faggot>
     {hwid = "DB539F46-DD89-4E1C-B379-5616B765E8E7", ip = "86.150.185.170", user_id = "123511235"}, --<123511235>
+    {hwid = "b51596b9-1ffe-4979-9b5a-b5f8b007f3e3", ip = "181.53.12.65", user_id = "idk"}, --<idk>
 })
