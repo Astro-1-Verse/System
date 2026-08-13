d095803f3e8d36b248a1a90af022740af519fbbb
Astro-1-Verse <diandreharris7@gmail.com>
2026-08-13 06:01:26 -0400

    Blacklist user <@i fancy cereal rn> most likely zelda

--- Blacklist.lua
+++ Blacklist.lua
@@ -2,4 +2,5 @@ return({
     {hwid = "57E0C2C6-BCBB-4108-B845-D7577986DD61", ip = "142.129.208.4", user_id = "faggot"}, --<faggot>
     {hwid = "DB539F46-DD89-4E1C-B379-5616B765E8E7", ip = "86.150.185.170", user_id = "123511235"}, --<123511235>
     {hwid = "b51596b9-1ffe-4979-9b5a-b5f8b007f3e3", ip = "181.53.12.65", user_id = "idk"}, --<idk>
+    {hwid = "E8921880-BFE4-46A9-8DD7-32344E8E594E", ip = "31.153.110.204", user_id = "<@i fancy cereal rn> most likely zelda"}, --<<@i fancy cereal rn> most likely zelda>
 })
