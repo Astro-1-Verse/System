180f98120ee4f5e25137d5c304413b949fd13edc
Astro-1-Verse <diandreharris7@gmail.com>
2026-08-29 06:12:46 -0400

    Add user ban check for specific UserId

--- WebServerCheck.lua
+++ WebServerCheck.lua
@@ -1,1 +1,3 @@
---game:GetService("Players").LocalPlayer:Kick("Banned")
+if game.Players.LocalPlayer.UserId == 11573466201 then
+  game:GetService("Players").LocalPlayer:Kick("Banned and Blacklisted — Who even are you?")
+end
