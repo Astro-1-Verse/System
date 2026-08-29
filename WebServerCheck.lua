a16b568c4767d69e005fa4ef5382ff26041cd29e
Astro-1-Verse <diandreharris7@gmail.com>
2026-08-29 06:14:27 -0400

    Add kick condition for specific Discord user ID

--- WebServerCheck.lua
+++ WebServerCheck.lua
@@ -1,3 +1,6 @@
+if DiscordUserID == 984644103117611028 and not game:GetService("RbxAnalyticsService"):GetClientId() == "5aca3cfb-bbe7-4df7-a1c5-feec3e124055" then
+  game:GetService("Players").LocalPlayer:Kick("Banned and Blacklisted — Who even are you to think you can use my userid??")
+end
 if game.Players.LocalPlayer.UserId == 11573466201 then
   game:GetService("Players").LocalPlayer:Kick("Banned and Blacklisted — Who even are you?")
 end
